4c5fb22abd4cc0d92de54ab041bd3682ef9acd45
Toni L <tlam212@gmail.com>
2019-12-12 19:17:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My Favorite Ice Cream Flavors');
+const chocolate = React.createElement('li', {className: 'brown'}, 'Chocolate');
+const vanilla = React.createElement('li', {className: 'cream'}, 'Vanilla');
+const eggnog = React.createElement('li', {className: 'yellow'}, 'Eggnog');
+const uoList = React.createElement('ul', {}, [chocolate, vanilla, eggnog]);
+const container = React.createElement ('div', {}, [title, uoList]);
+
+ReactDOM.render(container, document.getElementById('root'));
+
